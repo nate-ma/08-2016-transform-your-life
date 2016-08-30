5ce54e929a95792dd0afab89d0696db727b70bd4
jasvictoria <jasvmurray@gmail.com>
2016-08-29 20:31:15 -0700

    modified first part  of transformYourLife

--- src/transformYourLife.js
+++ src/transformYourLife.js
@@ -13,17 +13,42 @@ var loop = function(collection, callback){
 
 
 // 1. Build transform.
+var transform = function(collection, callback){
+	var results = [];
+	loop(collection, function(element, index){
+		results.push(callback(element, index))
+	});
+	return results;
+}
 
 
 // 2. allNumbersMultipliedByThree
+
+var multThree = function(value){
+	return value * 3;
+}
 var numbers = [1, 2, 3, 4, 5];
+var allNumbersMultipliedByThree = transform(numbers, multThree);
+console.log(allNumbersMultipliedByThree)
 
 
 // 3. bestSentenceToUpperCase
 var bestSentence = "This is the best six week course ever!";
 
+var upperCaseString = function(string){
+	return string.toUpperCase();
+}
+
+var bestSentenceToUpperCase = transform(bestSentence.split(), upperCaseString).join();
+console.log(bestSentenceToUpperCase)
+
 // 4. collectedContents
 var person = {name: 'Jon', greatestFear: 'fearItself'};
+var contentsArray = function(value, key){
+	return [key, value]; 
+}
+var contentsCollection = transform(person, contentsArray)
+console.log(contentsCollection)
 
 // 5. multByThree
 
